3965f1dcc048f2dbf23e7a6ee04b0b0c899d8f58
shaghaf7 <shaghafkashif@gmail.com>
2024-11-03 13:12:23 +0530

    Home component added

---
+++ src/components/Home.jsx
@@ -0,0 +1,31 @@
+import React from 'react'
+import { heroImage } from "../assets/heroImage.png";
+import { MdArrowRightAlt } from "react-icons/md";
+
+
+const Home = () => {
+  return (
+    <div>
+     
+     <div>
+        <div>
+            <h2>
+                I'm a Full Stack Developer
+            </h2>
+            <p>
+
+            </p>
+            <div>
+                <button>
+                    Portfolio<span>
+                        <MdArrowRightAlt/>
+                    </span>
+                </button>
+            </div>
+        </div>
+     </div>
+    </div>
+  )
+}
+
+export default Home
