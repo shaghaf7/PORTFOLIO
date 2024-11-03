165ad39953e667f754bac65adde76d5e052b0a13
shaghaf7 <shaghafkashif@gmail.com>
2024-11-03 16:43:24 +0530

    added git ignore and updated app.js file

---
+++ src/App.js
@@ -0,0 +1,18 @@
+
+import './App.css';
+import NavBar from './components/NavBar';
+import Home from './components/Home';
+import SocialLinks from './components/SocialLinks';
+function App() {
+  return (
+    <div>
+     <NavBar/>
+     <Home/>
+
+     
+     <SocialLinks/>
+    </div>
+  );
+}
+
+export default App;
