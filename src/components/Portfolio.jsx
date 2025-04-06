7d51d0a221362e82a7a2c1de9eeb2adbe783d5b8
shaghaf7 <shaghafkashif@gmail.com>
2025-04-06 16:21:25 +0530

    changes Portfoilio section

---
+++ src/components/Portfolio.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+
+const Portfolio = () => {
+  return (
+    <div name="portfolio" className='bg-gradient-to-b from-black to-gray-800 w-full text-white md:h-screen'>
+      <div>
+        <div >
+            <p>Portfolio</p>
+            <p>Check out some of my work right here</p>
+        </div>
+        <div>
+
+            <div>
+                <img src='' alt='' />
+                <div>
+                    <button>Demo</button>
+                    <button>Code</button> 
+                </div>
+            </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Portfolio
